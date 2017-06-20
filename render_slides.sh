adffe2634b358178a676694a762f12af36fa1682
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-06-20 10:08:20 -0400

    even more things

--- render_slides.sh
+++ render_slides.sh
@@ -4,7 +4,7 @@ git commit -m "fresh render "
 git push origin master
 git checkout gh-pages
 git checkout master -- index.html
-git add index.html
+git add -A
 git commit -m "fresh render on index.html"
 git push origin gh-pages
 git checkout master
